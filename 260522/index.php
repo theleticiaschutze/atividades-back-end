2725800575107d09ed321bd0645f6315c2ad181f
Letícia Schütze <the.leticia.schutze@gmail.com>
2026-05-22 17:19:57 -0300

    Atividade de hoje com icones do font awesome

---
+++ 260522/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/all.min.css" rel="stylesheet">
+    <title>Chamando icones do font awesome</title>
+</head>
+
+<body>
+    <i class="fa-solid fa-circle-radiation"></i>
+    <i class="fa-regular fa-heart"></i>
+    <i class="fa-solid fa-fish"></i>
+    <i class="fa-brands fa-css"></i>
+    <i class="fa-brands fa-bootstrap"></i>
+    <i class="fa-brands fa-github"></i>
+    <i class="fa-brands fa-html5"></i>
+    <i class="fa-solid fa-paw"></i>
+    <i class="fa-solid fa-paw fa-2xl"></i>&nbsp;Paw
+    <i class="fa-solid fa-otter"></i>
+</body>
+
+</html>  
